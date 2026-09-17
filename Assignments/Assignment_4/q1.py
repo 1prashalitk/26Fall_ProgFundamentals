0d883dd9395fd3292a3c2a547ae3c3a662351e45
Thakur <PThakur@twu.edu>
2026-09-17 10:15:52 -0500

    Created Assignment 4 files

---
+++ Assignments/Assignment_4/q1.py
@@ -0,0 +1,4 @@
+Name="Chia"
+Age=7
+Height=122
+print(Name,"is",Age,"years old and is",Height,"m tall.")
---
+++ Assignments/Assignment_4/q2.py
@@ -0,0 +1,6 @@
+length=12.5
+width=4
+area=length*width
+perimeter= 2*(length+width)
+print(f"Area of the rectangle is: {area:.2f}")
+print(f"Perimeter of rectangle is: {perimeter:.2f}")
