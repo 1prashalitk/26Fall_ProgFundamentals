8674fec858f4edf6f7738ecd3c74fc76439efe0c
Thakur <PThakur@twu.edu>
2026-09-17 10:33:26 -0500

    Created Assignment 4 files

--- Assignments/Assignment_4/q2.py
+++ Assignments/Assignment_4/q2.py
@@ -4,3 +4,4 @@ area=length*width
 perimeter= 2*(length+width)
 print(f"Area of the rectangle is: {area:.2f}")
 print(f"Perimeter of rectangle is: {perimeter:.2f}")
+    
---
+++ Assignments/Assignment_4/q3.py
@@ -0,0 +1,8 @@
+Sarah= 99
+Robert= 84
+Victor= 90
+Total= Sarah+Robert+Victor
+Average= (Total/300)*100
+print("Total:", Total, "out of 300")
+print(f"Average:{Average:.2f}%")
+
